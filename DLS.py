bda866573c35f02b96b8fd0c36f2aa980f7ddfab
kjammes <jayeshkarli@gmail.com>
2020-11-10 16:29:03 +0530

    Adding final version of DLS with path available

--- DLS.py
+++ DLS.py
@@ -8,33 +8,31 @@ graph = {
     'G':[],
 }
 
-path = ["A"]
-
-def DLS(start,goal,level,maxD):
+def DLS(start,goal,path,level,maxD):
     print("\nWe're on level",level)
     print("Goal node testing for",start)
     path.append(start)
     if start==goal:
         print("Goal node test successful!\n")
-        return True
-    else:
-        if path:
-            path.remove(path[-1])
+        return path
     print("Goal node testing failed")
     if level==maxD:
         return False
     print("\nExpanding node-->",start)
     for child in graph[start]:
-        result = DLS(child,goal,level+1,maxD)
+        result = DLS(child,goal,path,level+1,maxD)
         if result:
             return result
+        else:
+            path.pop()
     return False
 
 start = 'A'
 goal = input('Enter the goal node:-')
 maxD = int(input("Enter the maximum depth limit:-"))
 print()
-res = DLS('A',goal,0,maxD)
+path = list()
+res = DLS('A',goal,path,0,maxD)
 if(res):
     print("Path to goal node available")
     print("Path",path)
